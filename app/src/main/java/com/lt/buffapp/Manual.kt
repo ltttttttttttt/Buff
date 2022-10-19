ad94b7697adc88f1edd8b49fb7ca5f234b829547
lt <lt.dygzs@qq.com>
2022-10-19 18:19:34 +0800

    Update Manual.kt

--- app/src/main/java/com/lt/buffapp/Manual.kt
+++ app/src/main/java/com/lt/buffapp/Manual.kt
@@ -1,7 +1,12 @@
 package com.lt.buffapp
 
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 /**
  * creator: lt  2022/10/19  lt.dygzs@qq.com
@@ -10,5 +15,31 @@ import androidx.compose.runtime.Composable
  */
 @Composable
 fun ColumnScope.Manual() {
+    val bean = remember {
+        """{"id":1,"name":"Manual"}""".jsonToAny<ManualBean>()
+    }
+    Text(text = "手动方式,id=${bean.id}")
+    TextField(value = bean.name ?: "", onValueChange = { bean.name = it })
+    Button(onClick = {
+        bean.toJson().showToast()
+    }) {
+        Text(text = "show json")
+    }
+}
 
+@kotlinx.serialization.Serializable
+data class ManualBean(
+    val id: Int? = null,
+) {
+    @kotlinx.serialization.Transient
+    val nameState = mutableStateOf<String?>(null)
+    var name: String? = null
+        get() {
+            nameState.value = field
+            return nameState.value
+        }
+        set(value) {
+            field = value
+            nameState.value = value
+        }
 }
