1b58202b524d5d3d2a674e1fcab4c7fab2d2ea34
lt <lt.dygzs@qq.com>
2022-10-21 17:58:11 +0800

    add jitpack

--- buff/build.gradle.kts
+++ buff/build.gradle.kts
@@ -1,11 +1,28 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    id("maven-publish")
 }
 
+group = "com.github.ltttttttttttt"
+version = "1.0.0"
+
 java {
+    withSourcesJar()
+    withJavadocJar()
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    publications {
+        create("maven_public", MavenPublication::class) {
+            groupId = "com.github.ltttttttttttt"
+            artifactId = "library"
+            version = "1.0.0"
+            from(components.getByName("java"))
+        }
+    }
 }
 
 dependencies {
