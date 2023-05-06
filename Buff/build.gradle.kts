618cb9059b0cf2a3c1f6eca23554f5515f7d07d4
lt <lt.dygzs@qq.com>
2023-05-06 17:51:52 +0800

    move

---
+++ Buff/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform")
+    id("convention.publication")
+}
+
+group = "io.github.ltttttttttttt"
+//上传到mavenCentral命令: ./gradlew publishAllPublicationsToSonatypeRepository
+//mavenCentral后台: https://s01.oss.sonatype.org/#stagingRepositories
+version = mVersion
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting
+
+        val jvmMain by getting {
+            dependencies {
+                implementation("com.google.devtools.ksp:symbol-processing-api:$kspVersion")
+                implementation(project(":Buff-lib"))
+            }
+        }
+        val jvmTest by getting
+    }
+}
