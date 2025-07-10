5905b117e707061b107413e3702798282f49f002
lt <lt.dygzs@qq.com>
2025-07-10 15:34:15 +0800

    使用新的发版方式

---
+++ buildSrc/src/main/kotlin/PublishConfig.kt
@@ -0,0 +1,12 @@
+/**
+ * creator: lt  2025/7/10  lt.dygzs@qq.com
+ * effect :
+ * warning:
+ */
+object PublishConfig {
+    val description = "Add status to beans in Compose, Fields in beans can be directly used as the MutableState<T>"//项目描述
+    val inceptionYear = "2022"//项目开始的年份
+    val projectUrl = "https://github.com/ltttttttttttt/Buff"//项目地址
+
+    val group = "io.github.ltttttttttttt"
+}
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,5 +2,6 @@ const val kotlinVersion = "2.1.21"//kotlin版本
 const val composeVersion = "1.8.0"//compose版本
 //const val composeCompilerVersion = "1.4.5"//compose编译版本
 const val kspVersion = "$kotlinVersion-2.0.1"//ksp版本
+const val publishVersion = "0.33.0"
 
-const val mVersion = "1.2.1"//此库的版本
+const val mVersion = "1.2.2"//此库的版本
